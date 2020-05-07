a05a3200d3382f7f5fc9cb1c02845173456f8cf9
pepp-99 <g.cardaropoli15@studenti.unisa.it>
2020-05-07 17:46:55 +0200

    Aggiunta nome in popolamento.sql

--- WEBAPP/DB/popolamento.sql
+++ WEBAPP/DB/popolamento.sql
@@ -1,18 +1,17 @@
-insert into StrutturaAlberghiera (id, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
-	values ('39867a1b-eb84-4fe6-9c80-4d973c6fa3b7', 'via roma 17', 267, 3, 'https://bit.ly/3fpCl8h', 'pbrealey0@skype.com', '007145321');
+insert into StrutturaAlberghiera (id, nome, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
+	values ('39867a1b-eb84-4fe6-9c80-4d973c6fa3b7', 'hotel ercolino', 'via roma 17', 267, 3, 'https://bit.ly/3fpCl8h', 'pbrealey0@skype.com', '007145321');
      
-insert into StrutturaAlberghiera (id, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
-	values ('e583bfa8-027e-4027-a66a-64bf8c05d86d', 'corso armando 29', 261, 2, 'https://bit.ly/3dozwlR', 'hbortolussi1@rediff.com', '074722777');
+insert into StrutturaAlberghiera (id, nome, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
+	values ('e583bfa8-027e-4027-a66a-64bf8c05d86d', 'hotel rosa', 'corso armando 29', 261, 2, 'https://bit.ly/3dozwlR', 'hbortolussi1@rediff.com', '074722777');
     
-insert into StrutturaAlberghiera (id, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
-	values ('9391828e-ad7c-4aec-a6aa-111d15a5f575', 'via garibaldi 10', 285, 4, 'https://bit.ly/3dqn70I', 'bbaudon2@hatena.ne.jp', '021640460');
+insert into StrutturaAlberghiera (id, nome, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
+	values ('9391828e-ad7c-4aec-a6aa-111d15a5f575', 'hotel dei venti', 'via garibaldi 10', 285, 4, 'https://bit.ly/3dqn70I', 'bbaudon2@hatena.ne.jp', '021640460');
     
-insert into StrutturaAlberghiera (id, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
-	values ('a454c50e-bf08-45c1-bc20-4ea80aa8ed1c', 'via scotti 9', 166, 5, 'https://bit.ly/35FwoiR', 'dcotterrill3@un.org', '003180304');
+insert into StrutturaAlberghiera (id, nome, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
+	values ('a454c50e-bf08-45c1-bc20-4ea80aa8ed1c', 'hotel genoveffa', 'via scotti 9', 166, 5, 'https://bit.ly/35FwoiR', 'dcotterrill3@un.org', '003180304');
     
-insert into StrutturaAlberghiera (id, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
-	values ('d9d2500f-5312-45a9-ad66-f4553685043c', 'via corte pergamo 33', 251, 4, 'https://bit.ly/2A8VdYT', 'lgeorgins4@last.fm', '040786291');
+insert into StrutturaAlberghiera (id, nome, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
+	values ('d9d2500f-5312-45a9-ad66-f4553685043c', 'hotel stella', 'via corte pergamo 33', 251, 4, 'https://bit.ly/2A8VdYT', 'lgeorgins4@last.fm', '040786291');
 
-
-insert into StrutturaAlberghiera (id, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
-	values ('344f39a7-066c-4bcf-bfa0-199528a42125', 'via nasto 22', 91, 3, 'https://bit.ly/2SJ7MAE', 'rbartholomew5@freewebs.com', '068099637');
+insert into StrutturaAlberghiera (id, nome, indirizzo, costoNotte, stelle, immagine, email, numeroTelefono)
+	values ('344f39a7-066c-4bcf-bfa0-199528a42125', 'hotel sabaku', 'via nasto 22', 91, 3, 'https://bit.ly/2SJ7MAE', 'rbartholomew5@freewebs.com', '068099637');
