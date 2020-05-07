4759cc77450da1e95aea240a8a2704dcb8351909
pepp-99 <g.cardaropoli15@studenti.unisa.it>
2020-05-07 20:03:55 +0200

    Modifica RestaurantBean (aggiunta città)

--- WEBAPP/src/model/restaurant/RestaurantBean.java
+++ WEBAPP/src/model/restaurant/RestaurantBean.java
@@ -6,6 +6,7 @@ public class RestaurantBean {
 
     private UUID id;
     private String indirizzo;
+    private String citta;
     private String nome;
     private double costo;
     private String immagine;
@@ -14,6 +15,16 @@ public class RestaurantBean {
 
     public RestaurantBean() { }
 
+    public RestaurantBean(UUID id, String indirizzo, String citta, String nome, double costo, String immagine, String numeroTelefono, String email) {
+        this.id = id;
+        this.indirizzo = indirizzo;
+        this.citta = citta;
+        this.nome = nome;
+        this.costo = costo;
+        this.immagine = immagine;
+        this.numeroTelefono = numeroTelefono;
+        this.email = email;
+    }
 
     public UUID getId() {
         return id;
@@ -27,23 +38,15 @@ public class RestaurantBean {
         return indirizzo;
     }
 
-    public RestaurantBean(UUID id, String indirizzo, String nome, double costo, String immagine, String numeroTelefono, String email) {
-        this.id = id;
-        this.indirizzo = indirizzo;
-        this.nome = nome;
-        this.costo = costo;
-        this.immagine = immagine;
-        this.numeroTelefono = numeroTelefono;
-        this.email = email;
-    }
-
     public void setIndirizzo(String indirizzo) {
         this.indirizzo = indirizzo;
     }
 
-    public String getNome() {
-        return nome;
-    }
+    public String getCitta() { return citta; }
+
+    public void setCitta(String citta) { this.citta = citta; }
+
+    public String getNome() { return nome; }
 
     public void setNome(String nome) {
         this.nome = nome;
@@ -80,6 +83,4 @@ public class RestaurantBean {
     public void setEmail(String email) {
         this.email = email;
     }
-
-
 }
