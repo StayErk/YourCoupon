d988ad1bee33999629c6c2e440eff6ea192c9e72
mariachiara <m.nasto6@studenti.unisa.it>
2020-05-08 23:38:32 +0200

    cambiamenti vari ed eventuali pt2

--- WEBAPP/src/model/tour/LuogoTester.java
+++ WEBAPP/src/model/tour/LuogoTester.java
@@ -37,11 +37,38 @@ public class LuogoTester {
         }
 
         System.out.println("retrivebykey");
-         try {
+        try {
             System.out.println("risultato query: " + modelDAO.retrieveByKey(UUID.fromString("9089b21c-6e76-419f-8059-aa3fdc92f9ba")));
         } catch (SQLException e) {
             System.out.println("SQL EXCEPTION su retrivebykey");
             e.printStackTrace();
         }
+
+        System.out.println("doSave");
+        try {
+            LuogoBean toInsert = new LuogoBean(UUID.randomUUID(), "Luogo Test", "Via Test", "MSS", "vabbe balliamo", "https://bit.ly/2A8VdYT");
+            System.out.println(modelDAO.retrieveAll("", "").size());
+            modelDAO.doSave(toInsert);
+            System.out.println(toInsert + "è stato inserito");
+            System.out.println(modelDAO.retrieveAll("", "").size());
+        } catch (SQLException e) {
+            System.out.println("SQL EXCEPTION su doSave");
+            e.printStackTrace();
+        }
+
+        System.out.println("doDelete");
+        try {
+            System.out.println("cancello il luogo con l'id: c99f7bea-0b29-4344-be9b-4595a930a4a0");
+            System.out.println(modelDAO.retrieveAll("", "").size());
+            modelDAO.doDelete((LuogoBean) modelDAO.retrieveAll("","").get(0));
+            System.out.println("cancellato?");
+            System.out.println(modelDAO.retrieveAll("", "").size());
+            System.out.println("che culo funziona");
+        } catch (SQLException e) {
+            System.out.println("SQL EXCEPTION su doDelete");
+            e.printStackTrace();
+        }
+
+
     }
 }
