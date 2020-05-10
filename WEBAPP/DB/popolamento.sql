e1b02966d7a3aee665ce96ea07a31fa865ff8333
pepp-99 <g.cardaropoli15@studenti.unisa.it>
2020-05-10 10:54:35 +0200

    Debug popolamento ristoranti

--- WEBAPP/DB/popolamento.sql
+++ WEBAPP/DB/popolamento.sql
@@ -1,16 +1,16 @@
 /* Popolamento Test StrutturaAlberghiera */
 insert into StrutturaAlberghiera (id, nome, indirizzo, citta, costoNotte, stelle, immagine, email, numeroTelefono)
 values ('39867a1b-eb84-4fe6-9c80-4d973c6fa3b7', 'hotel ercolino', 'via roma 17', 'Roma', 267, 3, 'https://bit.ly/3fpCl8h', 'pbrealey0@skype.com', '007145321');
-     
+
 insert into StrutturaAlberghiera (id, nome, indirizzo, citta, costoNotte, stelle, immagine, email, numeroTelefono)
 values ('e583bfa8-027e-4027-a66a-64bf8c05d86d', 'hotel rosa', 'corso armando 29', 'Firenze', 261, 2, 'https://bit.ly/3dozwlR', 'hbortolussi1@rediff.com', '074722777');
-    
+
 insert into StrutturaAlberghiera (id, nome, indirizzo, citta, costoNotte, stelle, immagine, email, numeroTelefono)
 values ('9391828e-ad7c-4aec-a6aa-111d15a5f575', 'hotel dei venti', 'via garibaldi 10', 'Napoli', 285, 4, 'https://bit.ly/3dqn70I', 'bbaudon2@hatena.ne.jp', '021640460');
-    
+
 insert into StrutturaAlberghiera (id, nome, indirizzo, citta, costoNotte, stelle, immagine, email, numeroTelefono)
 values ('a454c50e-bf08-45c1-bc20-4ea80aa8ed1c', 'hotel genoveffa', 'via scotti 9', 'Bari', 166, 5, 'https://bit.ly/35FwoiR', 'dcotterrill3@un.org', '003180304');
-    
+
 insert into StrutturaAlberghiera (id, nome, indirizzo, citta, costoNotte, stelle, immagine, email, numeroTelefono)
 values ('d9d2500f-5312-45a9-ad66-f4553685043c', 'hotel stella', 'via corte pergamo 33', 'Milano', 251, 4, 'https://bit.ly/2A8VdYT', 'lgeorgins4@last.fm', '040786291');
 
@@ -19,22 +19,22 @@ values ('344f39a7-066c-4bcf-bfa0-199528a42125', 'hotel sabaku', 'via nasto 22', 
 
 /* Popolamento Test StruttureRistorative */
 insert into StruttureRistorative (id, indirizzo, citta, nome, costo, immagine, numeroTelefono, email)
-values ('3982a01f-f37a-4edf-883e-4f4f6333d4c9', 'via creta 18', 'La Pagliarella', 'Roma', 78, 'https://bit.ly/2YK6yZQ', '026801604', 'lcarah0@netlog.com');
+values ('3982a01f-f37a-4edf-883e-4f4f6333d4c9', 'via creta 18', 'Roma', 'La Pagliarella', 78, 'https://bit.ly/2YK6yZQ', '026801604', 'lcarah0@netlog.com');
 
 insert into StruttureRistorative (id, indirizzo, citta, nome, costo, immagine, numeroTelefono, email)
-values ('42b7a32e-c4c6-4892-9ab7-deb52b8ac23c', 'via napoli 11', 'Il Paperone', 'Firenze', 97, 'https://bit.ly/3ceYt2W', '028290174', 'khugnin1@mashable.com');
+values ('42b7a32e-c4c6-4892-9ab7-deb52b8ac23c', 'via napoli 11', 'Firenze', 'Il Paperone', 97, 'https://bit.ly/3ceYt2W', '028290174', 'khugnin1@mashable.com');
 
 insert into StruttureRistorative (id, indirizzo, citta, nome, costo, immagine, numeroTelefono, email)
-values ('bb15dd0a-9c8c-4963-a883-67fb123a33a3', 'via dio 69', 'Made in Italy', 'Napoli', 77, 'https://bit.ly/2SK1sZo', '013949988', 'pjenner2@xrea.com');
+values ('bb15dd0a-9c8c-4963-a883-67fb123a33a3', 'via dio 69', 'Napoli', 'Made in Italy', 77, 'https://bit.ly/2SK1sZo', '013949988', 'pjenner2@xrea.com');
 
 insert into StruttureRistorative (id, indirizzo, citta, nome, costo, immagine, numeroTelefono, email)
-values ('cb72503c-de77-4bf8-af2c-4033f86d54da', 'via toffo 3', 'Rosso Pomodoro', 'Bari', 45, 'https://bit.ly/2YIahac', '064181984', 'jcrocken3@admin.ch');
+values ('cb72503c-de77-4bf8-af2c-4033f86d54da', 'via toffo 3', 'Bari', 'Rosso Pomodoro', 45, 'https://bit.ly/2YIahac', '064181984', 'jcrocken3@admin.ch');
 
 insert into StruttureRistorative (id, indirizzo, citta, nome, costo, immagine, numeroTelefono, email)
-values ('af34c083-7d59-49aa-98bf-8e7180cb023a', 'via carda 77', 'Vecchio mulino', 'Milano', 13, 'https://bit.ly/2Wy9FRG', '007241321', 'sgabbot4@washington.edu');
+values ('af34c083-7d59-49aa-98bf-8e7180cb023a', 'via carda 77', 'Milano', 'Vecchio mulino', 13, 'https://bit.ly/2Wy9FRG', '007241321', 'sgabbot4@washington.edu');
 
 insert into StruttureRistorative (id, indirizzo, citta, nome, costo, immagine, numeroTelefono, email)
-values ('272ca703-b314-408b-aa64-f8d5cb2f0b56', 'via fisciano 28', 'Purchiacca di mare', 'Angri', 85, 'https://bit.ly/3ccPeAh', '006225957', 'bscopyn5@canalblog.com');
+values ('272ca703-b314-408b-aa64-f8d5cb2f0b56', 'via fisciano 28', 'Angri', 'Purchiacca di mare', 85, 'https://bit.ly/3ccPeAh', '006225957', 'bscopyn5@canalblog.com');
 
 /* Popolamento Test Luogo */
 insert into Luogo (id, nome, indirizzo, citta, descrizione, immagine)
