340afcb7170813b921fb777c43f8dcf9563379a1
mariachiara <m.nasto6@studenti.unisa.it>
2020-05-14 11:04:26 +0200

    retrieveByKey, mapFromResulteSet

--- WEBAPP/src/model/carrello/CarrelloDAO.java
+++ WEBAPP/src/model/carrello/CarrelloDAO.java
@@ -1,14 +1,39 @@
 package model.carrello;
 
+import datasource.DriverManagerConnectionPool;
 import model.ComponentCRUD;
+import model.cliente.ClienteBean;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 public class CarrelloDAO implements ComponentCRUD<CarrelloBean, String> {
     @Override
     public CarrelloBean retrieveByKey(String key) throws SQLException {
-        return null;
+        String sql ="SELECT * FROM Carrello WHERE id=?";
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        CarrelloBean carrello;
+
+        try{
+            connection = DriverManagerConnectionPool.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,key);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            carrello = mapFromResultSet(rs);
+        }
+        finally {
+            try{
+                if(preparedStatement != null) preparedStatement.close();
+            } finally {
+                DriverManagerConnectionPool.releaseConnection(connection);
+            }
+        }
+        return carrello;
     }
 
     @Override
@@ -23,11 +48,21 @@ public class CarrelloDAO implements ComponentCRUD<CarrelloBean, String> {
 
     @Override
     public void doUpdate(CarrelloBean objectToUpdate) throws SQLException {
-
+        String sql = "";
     }
 
     @Override
     public void doDelete(CarrelloBean objectToDelete) throws SQLException {
 
+    }
+
+    private CarrelloBean mapFromResultSet(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            return new CarrelloBean(
+              rs.getString("id_carrello"),
+              rs.getDouble("totale")
+            );
+        }
+        return new CarrelloBean();
     }
 }
