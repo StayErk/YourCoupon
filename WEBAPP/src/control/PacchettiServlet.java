503663a5dfbebb14a961ae8ca8c31ecfd931e37d
StayErk <stayerk@github.com>
2020-05-15 20:06:13 +0200

    Tester Carrello (A Fisciano tira vento)

--- WEBAPP/src/control/PacchettiServlet.java
+++ WEBAPP/src/control/PacchettiServlet.java
@@ -2,11 +2,12 @@ package control;
 
 import model.pacchetto.PacchettoDAO;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "PacchettiServlet")
+@WebServlet("/PacchettiServlet")
 public class PacchettiServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
@@ -15,15 +16,20 @@ public class PacchettiServlet extends javax.servlet.http.HttpServlet {
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         PacchettoDAO modelDAO = new PacchettoDAO();
         String action = request.getParameter("action");
+        System.out.println(action);
         switch (action){
             case "retrieve":
                 try {
-                    request.setAttribute("pacchetti", modelDAO.retrieveAll("costo", "ASC"));
+                    request.setAttribute("pacchetti", modelDAO.retrieveAll("", ""));
+                    System.out.println(modelDAO.retrieveAll("", ""));
                 } catch (SQLException e) {
-                    response.setStatus(500);
+                    request.setAttribute("errore", e.toString());
                 }
                 break;
         }
+
+        RequestDispatcher requestDispatcher = this.getServletContext().getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(request, response);
 
     }
 }
