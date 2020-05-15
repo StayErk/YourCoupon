865f9e0af03f737a77a8894309616587a9b497e9
mariachiara <m.nasto6@studenti.unisa.it>
2020-05-15 20:41:17 +0200

    servlet

--- WEBAPP/src/control/PacchettiServlet.java
+++ WEBAPP/src/control/PacchettiServlet.java
@@ -1,11 +1,16 @@
 package control;
 
+import model.hotel.HotelDAO;
+import model.pacchetto.PacchettoBean;
 import model.pacchetto.PacchettoDAO;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 @WebServlet("/PacchettiServlet")
 public class PacchettiServlet extends javax.servlet.http.HttpServlet {
@@ -15,12 +20,31 @@ public class PacchettiServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         PacchettoDAO modelDAO = new PacchettoDAO();
+        HotelDAO hotelDAO = new HotelDAO();
         String action = request.getParameter("action");
         System.out.println(action);
         switch (action){
             case "retrieve":
                 try {
-                    request.setAttribute("pacchetti", modelDAO.retrieveAll("", ""));
+                    HashMap<UUID, ArrayList<String>> hashPacchetti = new HashMap<>();
+                    ArrayList<String> info = new ArrayList<>();
+                    ArrayList<PacchettoBean> pacchetti = new ArrayList<>(modelDAO.retrieveAll("",""));
+                    for(PacchettoBean p: pacchetti){
+
+                        info.add(p.isPredefinito() + "");
+                        info.add(p.getCosto() + "");
+                        info.add(p.getDurata() + "");
+                        info.add(p.getPersone() + "");
+                        info.add(hotelDAO.retrieveByKey((p.getId_struttura())).getNome());
+                        info.add(hotelDAO.retrieveByKey((p.getId_struttura())).getCitta());
+                        info.add(hotelDAO.retrieveByKey((p.getId_struttura())).getStelle() + "");
+                        info.add(hotelDAO.retrieveByKey((p.getId_struttura())).getImmagine());
+
+                        hashPacchetti.put(p.getId(),info);
+                        info.clear();
+                    }
+
+                    request.setAttribute("pacchetti", hashPacchetti);
                     System.out.println(modelDAO.retrieveAll("", ""));
                 } catch (SQLException e) {
                     request.setAttribute("errore", e.toString());
--- WEBAPP/web/components/kinder-cards/_kinder-cards.jsp
+++ WEBAPP/web/components/kinder-cards/_kinder-cards.jsp
@@ -2,7 +2,8 @@
 <%@ page import="model.tour.LuogoBean" %>
 <%@ page import="java.util.UUID" %>
 <%@ page import="model.pacchetto.PacchettoBean" %>
-<%@ page import="java.util.List" %><%--
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.HashMap" %><%--
   Created by IntelliJ IDEA.
   User: maria
   Date: 11/05/2020
@@ -20,7 +21,7 @@
     luoghi.add((new LuogoBean(UUID.fromString("239c2175-308b-4498-8fda-60eb1a93a78f"), "Neyhf", "6 Browning Drive", "Dingli", "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.Vivamus vel nulla eget eros elementum pellentesque.", "https://bit.ly/3cfVSpt")));
     luoghi.add((new LuogoBean(UUID.fromString("239c2175-308b-4498-8fda-60eb1a93a78f"), "Neyhf", "6 Browning Drive", "Dingli", "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.Vivamus vel nulla eget eros elementum pellentesque.", "https://bit.ly/3cfVSpt")));
     */
-    List<PacchettoBean> pacchetti = (List<PacchettoBean>) request.getAttribute("pacchetti");
+    HashMap<UUID, ArrayList<String>> pacchetti = (HashMap<UUID, ArrayList<String>>) request.getAttribute("pacchetti");
     Object errore =  request.getAttribute("errore");
     if(pacchetti == null && errore == null) {
         response.sendRedirect(response.encodeURL("PacchettiServlet?action=retrieve"));
@@ -33,8 +34,8 @@
 <div class="card-deck">
     <div class="row">
         <% if(pacchetti != null) { %>
-            <% for(PacchettoBean p: pacchetti){ %>
-                <% if(p.isPredefinito()) {%>
+            <% for(UUID p: pacchetti.keySet()){ %>
+                <% if(pacchetti.get(p).get(0).equals("true")) {%>
                     <div class="col-4 mb-3">
                         <div class="card">
                             <!--<img src="l.getImmagine()" class="card-img-top img-fluid h-50" alt="//l.getNome()">-->
