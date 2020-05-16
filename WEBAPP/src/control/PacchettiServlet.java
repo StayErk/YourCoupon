cd88e6aeb208cfd1051b2cb35c7d93263d619a66
StayErk <stayerk@github.com>
2020-05-16 18:54:14 +0200

    Servlet PacchettiServlet

--- WEBAPP/src/control/PacchettiServlet.java
+++ WEBAPP/src/control/PacchettiServlet.java
@@ -23,6 +23,7 @@ public class PacchettiServlet extends javax.servlet.http.HttpServlet {
         PacchettoDAO modelDAO = new PacchettoDAO();
         HotelDAO hotelDAO = new HotelDAO();
         String action = request.getParameter("action");
+        String path;
         switch (action){
             case "retrieve":
                 try {
@@ -34,37 +35,46 @@ public class PacchettiServlet extends javax.servlet.http.HttpServlet {
                         componenti.add(p);
                         componenti.add(hotelDAO.retrieveByKey(p.getId_struttura()));
                         hashPacchetti.put(p.getId(), (ArrayList<Bean>) componenti.clone());
-
                         System.out.println(componenti);
                         componenti.clear();
                     }
 
                     request.setAttribute("pacchetti", hashPacchetti);
-                    RequestDispatcher requestDispatcher = this.getServletContext().getRequestDispatcher("/index.jsp");
-                    requestDispatcher.forward(request, response);
+
+
+
                 } catch (SQLException e) {
                     request.setAttribute("errore", e.toString());
+                    System.out.println("Errore retrieve");
+                    e.printStackTrace();
                 }
                 break;
             case "bykey":
                 String key = request.getParameter("key");
+                System.out.println(key);
                 if(key != null && !key.equals("")){
                     try {
                         ArrayList<Bean> beans = new ArrayList<>();
                         PacchettoBean pacchettoBean = modelDAO.retrieveByKey(UUID.fromString(key));
                         beans.add(pacchettoBean);
                         beans.add(hotelDAO.retrieveByKey(pacchettoBean.getId_struttura()));
-                        request.setAttribute("beans", beans);
+                        request.setAttribute("arraybeans", beans);
+                        System.out.println("RETRIEVE BY KEY: " + beans);
                         RequestDispatcher requestDispatcher = this.getServletContext().getRequestDispatcher("/details.jsp");
                         requestDispatcher.forward(request, response);
+
                     } catch (SQLException e) {
+                        System.out.println("Errore retrieve");
                         request.setAttribute("errore", e.toString());
                     }
                 }
                 break;
+            case "create":
+
+                break;
         }
-
-
+        RequestDispatcher requestDispatcher = this.getServletContext().getRequestDispatcher("/index.jsp");
+        requestDispatcher.forward(request, response);
 
     }
 }
