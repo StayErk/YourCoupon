6641d6319d25329840989369f916d8e697e26516
StayErk <stayerk@github.com>
2020-06-27 19:37:17 +0200

    Aggiustati i filtri

--- WEBAPP/web/components/catalogbean/catalogbean.js
+++ WEBAPP/web/components/catalogbean/catalogbean.js
@@ -10,12 +10,12 @@ let cittaInserite = []
 
 if(uri.includes('admin')){
     request_uri = `../ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}`
-    request_uriASC = `../ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=ASC&filter=`
-    request_uriDESC = `../ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=DESC&filter=`
+    request_uriASC = `../ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=ASC&filter=costo`
+    request_uriDESC = `../ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=DESC&filter=costo`
 }else {
     request_uri = `ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}`
-    request_uriASC = `ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=ASC&filter=`
-    request_uriDESC = `ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=DESC&filter=`
+    request_uriASC = `ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=ASC&filter=costo`
+    request_uriDESC = `ComponentsPackServlet;jsessionid=${form.sessionid.value}?component=${document.title.toLowerCase()}&order=DESC&filter=costo`
 }
 
 
